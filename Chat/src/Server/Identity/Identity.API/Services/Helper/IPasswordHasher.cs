5097f7ff6931afe746fb7c1576b1bd739cf41b9c
Sardor <sardorstudent0618@gmail.com>
2024-05-11 02:29:40 +0500

    Password Hasher

---
+++ Chat/src/Server/Identity/Identity.API/Services/Helper/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.API.Services.Helper
+{
+    public interface IPasswordHasher
+    {
+        string Encrypt(string password, string salt);
+        bool Verify(string hash, string password, string salt);
+    }
+}
