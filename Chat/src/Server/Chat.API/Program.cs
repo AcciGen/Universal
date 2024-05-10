3b6f19a0dd635987444d1062c92dad3cf43977a2
Sardor <sardorstudent0618@gmail.com>
2024-05-10 23:53:42 +0500

    Chat Services

---
+++ Chat/src/Server/Chat.API/Program.cs
@@ -0,0 +1,36 @@
+using Chat.API.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region SignalR Services
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+
+
+#endregion
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
