657acc0cf31ac089a8bcb8a921756321eda4a027
Sardor <sardorstudent0618@gmail.com>
2024-05-11 02:32:05 +0500

    SignalR added and Chat hub Implemented

---
+++ Chat/src/Server/Chat/Chat.API/Program.cs
@@ -0,0 +1,85 @@
+using Chat.API.Hubs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region SignalR Services
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+#endregion
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials());
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        options =>
+        {
+            options.TokenValidationParameters = GetTokenValidationParameters(builder.Configuration);
+
+            options.Events = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = (context) =>
+                {
+                    if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                    {
+                        context.Response.Headers.Add("IsTokenExpired", "true");
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowOrigin");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
+
+
+
+static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+{
+    return new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidIssuer = configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:Audience"],
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"])),
+        ClockSkew = TimeSpan.Zero,
+    };
+}
